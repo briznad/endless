0478fd87a00837661fc804a37d28253334a5705d
Brad Mallow <briznad@gmail.com>
2013-03-22 19:12:06 -0400

    alpha

--- assets/js/local/do.js
+++ assets/js/local/do.js
@@ -2,55 +2,188 @@
 
 	document.ontouchmove = function (e) { // prevent default up/down page scrolling on iOS
 		e.preventDefault();
-	}
+	};
 
-	var frameHeight = null,
-		frameImgs = _.shuffle([
-			'abstract_arial.jpg',
-			'ants_in_line.jpg',
-			'babseball.jpg',
-			'baby_rhino.jpg',
-			'bike_sprinter.jpg',
-			'brandenburg_gate.jpg',
-			'city_scape.jpg',
-			'covered_face.jpg',
-			'derp_deer.jpg',
-			'flower_face.jpg',
-			'foggy_coast.jpg',
-			'giraffe.jpg',
-			'golden_gate.jpg',
-			'island_paradise.jpg',
-			'long_exposure.jpg',
-			'maze_pattern.jpg',
-			'old_trees.jpg',
-			'orchid.jpg',
-			'photoshoot_outtake.jpg',
-			'pretty_indian.jpg',
-			'runway_models.jpg',
-			'siamese_cat.jpg',
-			'small_bar.jpg',
-			'small_island.jpg',
-			'steak_salad.jpg',
-			'train_window.jpg',
-			'veggies.jpg',
-			'wilting_daisies.jpg'
-		]),
+	var $activePanels = null,
 		initFrameSize = function () {
 			if (window.innerWidth > 1024) { // if loading on a desktop reduce the height to allow for a pretty frame
-	    		frameHeight = window.innerHeight - 80;
+	    		var frameHeight = window.innerHeight - 80;
 	    	} else { // on a tablet use full available height
-	    		frameHeight = window.innerHeight;
+	    		var frameHeight = window.innerHeight;
 	    	}
 
 	    	$('.visible-frame, section').css('height', frameHeight);
+
+	    	location.hash = 'check1';
+	    	location.hash = 'endless';
+		},
+		frameImgs = _.shuffle([
+			'abstract_macro.jpg',
+			'angry_flower.jpg',
+			'baby_rhino.jpg',
+			'bar_art.jpg',
+			'baseball.jpg',
+			'beach_scene.jpg',
+			'bike_racer.jpg',
+			'bitters.jpg',
+			'blurry.jpg',
+			'blurry_lights.jpg',
+			'boogie_boarder.jpg',
+			'bread_loafs.jpg',
+			'bread_rolls.jpg',
+			'cast_iron_food.jpg',
+			'cityscape.jpg',
+			'cityscape2.jpg',
+			'copper_coil.jpg',
+			'crazy_cat.jpg',
+			'cute_dog.jpg',
+			'derp_deer.jpg',
+			'dinner_plate.jpg',
+			'disco_ball.jpg',
+			'dj.jpg',
+			'dominican_ladies.jpg',
+			'dutch_minivan.jpg',
+			'fake_blond.jpg',
+			'foggy_coast.jpg',
+			'gorilla_in_forest.jpg',
+			'indian_lady.jpg',
+			'line_of_trees.jpg',
+			'mangy_puppy.jpg',
+			'motorcross.jpg',
+			'ninja_kick.jpg',
+			'old_lady.jpg',
+			'old_man.jpg',
+			'orchid.jpg',
+			'pig.jpg',
+			'runway_models.jpg',
+			'shadowed_horse.jpg',
+			'small_bar.jpg',
+			'smoking_model.jpg',
+			'solitary_building.jpg',
+			'solo_island.jpg',
+			'sprinters.jpg',
+			'steak_salad.jpg',
+			'street_car.jpg',
+			'sushi.jpg',
+			'transistors.jpg',
+			'ubuntu_laptop.jpg',
+			'underwater_swimmer.jpg',
+			'vegas_ny.jpg',
+			'vegas_ny_night.jpg',
+			'veggies.jpg',
+			'water_drop.jpg',
+			'wet_glass.jpg',
+			'white_people.jpg',
+			'wilting_daisies.jpg'
+		]),
+		imgPlease = function () { // return an image and rotate the list
+			frameImgs.push(frameImgs.shift()); // remove the first element and add it to the end of the list
+			return frameImgs[0]; // supply the current first image in the list
+		},
+		theKnownWorld = {
+			'0x1': '',
+			'-1x0': '',
+			'0x0': '',
+			'1x0': '',
+			'0x-1': ''
+		},
+		activeFrame = {
+			coordX: 0,
+			coordY: 0
+		},
+		drawGrid = function () {
+
+			$('.frameUp').data('coord-x', activeFrame.coordX).data('coord-y', activeFrame.coordY + 1);
+			$('.frameLeft').data('coord-x', activeFrame.coordX - 1).data('coord-y', activeFrame.coordY);
+			$('.frameCenter').data('coord-x', activeFrame.coordX).data('coord-y', activeFrame.coordY);
+			$('.frameRight').data('coord-x', activeFrame.coordX + 1).data('coord-y', activeFrame.coordY);
+			$('.frameDown').data('coord-x', activeFrame.coordX).data('coord-y', activeFrame.coordY - 1);
+
+			$activePanels.each( function (index, item) {
+	    		var tempCoordString = $(this).data('coord-x') + 'x' + $(this).data('coord-y');
+
+	    		if (theKnownWorld[tempCoordString] == undefined || theKnownWorld[tempCoordString] == '') theKnownWorld[tempCoordString] = imgPlease(); // if the given coordinates don't yet have an image associated, set an image
+
+	    		$(this).css('background-image', 'url(assets/images/frames/' + theKnownWorld[tempCoordString] + ')');
+	    	});
+		},
+		doSwipe = function (swipeType) {
+
+			switch (swipeType) {
+				case 'swipeLeft':
+					activeFrame.coordX++;
+					break;
+				case 'swipeRight':
+					activeFrame.coordX--;
+					break;
+				case 'swipeUp':
+					activeFrame.coordY--;
+					break;
+				case 'swipeDown':
+					activeFrame.coordY++;
+					break;
+			}
+
+			$('.visible-frame').scrollTo('.' + swipeType, 1000, {
+				easing: 'easeOutExpo',
+				onAfter: function () {
+					$('.frameCenter').css('background-image', 'url(assets/images/frames/' + theKnownWorld[activeFrame.coordX + 'x' + activeFrame.coordY] + ')');
+					$('.visible-frame').scrollTo('.frameCenter', {
+						onAfter: function () {
+							drawGrid();
+							attachSwipeEvent();
+						}
+					});
+				}
+			});
+
+			preAssignImages();
+
+		},
+		preAssignImages = function () {
+			var neighborFrames = [];
+			_(3).times(function (n) {
+				n++;
+				neighborFrames.push((activeFrame.coordX + n) + 'x' + activeFrame.coordY);
+				neighborFrames.push((activeFrame.coordX - n) + 'x' + activeFrame.coordY);
+				neighborFrames.push(activeFrame.coordX + 'x' + (activeFrame.coordY + n));
+				neighborFrames.push(activeFrame.coordX + 'x' + (activeFrame.coordY - n));
+				neighborFrames.push((activeFrame.coordX + n) + 'x' + (activeFrame.coordY + n));
+				neighborFrames.push((activeFrame.coordX - n) + 'x' + (activeFrame.coordY - n));
+			});
+
+			$.each(neighborFrames, function (index, item) {
+	    		if (theKnownWorld[item] == undefined || theKnownWorld[item] == '') theKnownWorld[item] = imgPlease(); // if the given coordinates don't yet have an image associated, set an image
+	    	});
+		},
+		preFetchCount = 0,
+		preFetchImages = function () {
+
+			if (preFetchCount < frameImgs.length) preFetchNext();
+
+			function preFetchNext () {
+				var dummyImageObject = new Image();
+				dummyImageObject.src = 'assets/images/frames/' + frameImgs[preFetchCount];
+				dummyImageObject.onload = function () {
+					preFetchCount++;
+					t = setTimeout(preFetchImages, 1000);
+				};
+			}
+		},
+		attachSwipeEvent = function () {
+			$('body').one('swipeLeft swipeRight swipeUp swipeDown', function (e) {
+	    		doSwipe(e.type);
+	    	});
 		};
 
     /* when DOM is ready, bind some shit */
 
     $(function() {
 
+    	$activePanels = $('section').not('.placeholder');
+
     	// redirect to the "start" frame
-    	location.hash = 'start';
+    	location.hash = 'endless';
 
     	// initialize frame height
  		initFrameSize();
@@ -60,16 +193,16 @@
     		initFrameSize();
     	});
 
-    	// initialize frame images
-    	$('.frame').each( function (index, item) {
-    		$(this).css('background-image', 'url(assets/images/frames/' + frameImgs[index] + ')')
-    	});
+    	// draw the grid for the first time
+    	drawGrid();
 
- 		// register swipe events
-    	$('body').on('swipeLeft swipeRight swipeUp swipeDown', function (e) {
-    		console.log(e.type);
-    		$('body').css('background-image', 'url(http://lorempixel.com/g/342/' + parseInt((frameHeight + 1) / 3) + '/)');
-    	});
+ 		// register swipe event
+    	attachSwipeEvent();
+
+    	// once the page has fully loaded, run image prefetching
+    	$(window).load(function () {
+    		preFetchImages();
+		});
 
 	});
 
